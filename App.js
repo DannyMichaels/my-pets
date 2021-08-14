da5d971a6575e8dc9f2c0048392e22c939c7c0e9
Daniel Michael <itzdanielmichael@gmail.com>
2021-08-14 15:16:24 -0400

    update App.js

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+// the navigation prop is passed to every screen component
+const HomeScreen = ({ navigation }) => (
+  <View style={styles.container}>
+    <Text>Home Screen</Text>
+    <Button
+      title="Go To Details"
+      onPress={() => navigation.navigate('Details')}
+    />
+    <StatusBar style="auto" />
+  </View>
+);
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button title="Go To Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
